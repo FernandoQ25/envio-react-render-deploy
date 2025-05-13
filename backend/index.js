e5eb8fc15fb17970cf90d2c2211fd7dc2bfd9548
FernandoQ25 <frquinapallo1@espe.edu.ec>
2025-05-13 16:39:39 -0500

    arreglos de FRONTEND_URL

--- backend/index.js
+++ backend/index.js
@@ -1,31 +1,43 @@
-import express from 'express';
-import cors from 'cors';
-import pg from 'pg';
-import {DB_HOST, DB_DATABASE, DB_PASSWORD, DB_PORT, DB_USER, FRONTEND_URL, PORT} from './config.js';
+import express from "express";
+import cors from "cors";
+import pg from "pg";
+import {
+  DB_HOST,
+  DB_DATABASE,
+  DB_PASSWORD,
+  DB_PORT,
+  DB_USER,
+  FRONTEND_URL,
+  PORT,
+} from "./config.js";
 
 const app = express();
 const pool = new pg.Pool({
-    host: DB_HOST,
-    database: DB_DATABASE,
-    user: DB_USER,
-    password: DB_PASSWORD,
-    port: DB_PORT
+  host: DB_HOST,
+  database: DB_DATABASE,
+  user: DB_USER,
+  password: DB_PASSWORD,
+  port: DB_PORT,
 });
 
+app.use(
+  cors({
+    origin: FRONTEND_URL,
+  })
+);
 
-app.use(cors({
-    origin: FRONTEND_URL
-}));
+app.get("/ping", async (req, res) => {
+  const result = await pool.query("SELECT Now()");
 
-app.get('/ping', async(req, res) => {
+  res.send({
+    pong: result.rows[0].now,
+  });
+});
 
-    const result = await pool.query('SELECT Now()')
-
-    res.send({
-        pong: result.rows[0].now
-    });
+app.get("/", (req, res) => {
+  res.send("Backend activo y corriendo 🎉");
 });
 
 app.listen(PORT, () => {
-    console.log('Server is running on port 3001');
+  console.log("Server is running on port 3001");
 });
